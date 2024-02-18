16385e3f3cd1b7f0aca107ebd7fc20585a179b49
itsbsy <brijeshsinghyadav.9@gmail.com>
2024-02-18 20:05:32 +0530

    test

--- anagram.js
+++ anagram.js
@@ -16,25 +16,25 @@ function anagram(input){
     console.log(obj)
 }
 anagram("brijesh");
-// function areAnagrams(str1, str2) {
-//     // Remove non-alphanumeric characters and convert to lowercase
-//     const cleanStr1 = str1.toLowerCase();
-//     const cleanStr2 = str2.toLowerCase();
+function areAnagrams(str1, str2) {
+    // Remove non-alphanumeric characters and convert to lowercase
+    const cleanStr1 = str1.toLowerCase();
+    const cleanStr2 = str2.toLowerCase();
   
-//     // Sort the characters in the strings
-//     const sortedStr1 = cleanStr1.split('').sort().join('');
-//     const sortedStr2 = cleanStr2.split('').sort().join('');
+    // Sort the characters in the strings
+    const sortedStr1 = cleanStr1.split('').sort().join('');
+    const sortedStr2 = cleanStr2.split('').sort().join('');
   
-//     // Check if the sorted strings are equal
-//     return sortedStr1 === sortedStr2;
-//   }
+    // Check if the sorted strings are equal
+    return sortedStr1 === sortedStr2;
+  }
   
-//   // Example usage
-//   const word1 = "listen";
-//   const word2 = "silent";
+  // Example usage
+  const word1 = "listen";
+  const word2 = "silent";
   
-//   if (areAnagrams(word1, word2)) {
-//     console.log(`${word1} and ${word2} are anagrams.`);
-//   } else {
-//     console.log(`${word1} and ${word2} are not anagrams.`);
-//   }
+  if (areAnagrams(word1, word2)) {
+    console.log(`${word1} and ${word2} are anagrams.`);
+  } else {
+    console.log(`${word1} and ${word2} are not anagrams.`);
+  }
